d3384fb92dd12973cad3fcadee478c8e874347b2
Antônio Barros <antoniohlb@hotmail.com>
2024-04-19 18:40:12 -0300

    Pieces movement added

--- ChessExeption.java
+++ ChessExeption.java
@@ -1,4 +1,4 @@
-public class ChessExeption extends RuntimeException {
+public class ChessExeption extends BoardExeption {
     private static final long serialVersionUID = 1L;
 
     public ChessExeption(String msg) {
--- Program.java
+++ Program.java
@@ -1,9 +1,25 @@
+import java.util.Scanner;
+
 public class Program {
     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
 
         Board board = new Board(8, 8);
 
         ChessMatch chessMatch = new ChessMatch();
-        UI.printboard(chessMatch.getPieces());
+
+        while (true) {
+            UI.printboard(chessMatch.getPieces());
+            System.out.println();
+            System.out.print("Source: ");
+            ChessPosition source = UI.readChessPosition(sc);
+
+            System.out.println();
+            System.out.print("Target: ");
+            ChessPosition target = UI.readChessPosition(sc);
+
+            ChessPiece capturedPiece = chessMatch.peformChessMove(source, target);
+        }
     }
 }
