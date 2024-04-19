185d52622604e0f0e86522867d06f62a66dde9f9
Antônio Barros <antoniohlb@hotmail.com>
2024-04-19 16:10:44 -0300

    Added ChessExeption and ChessPosition

---
+++ BoardExeption.java
@@ -0,0 +1,8 @@
+public class BoardExeption extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+
+    public BoardExeption(String msg) {
+        super(msg);
+    }
+
+}
