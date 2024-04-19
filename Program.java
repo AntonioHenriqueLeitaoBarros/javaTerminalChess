4a20a382695b99b46f8401b3a800db672a9a80bb
Antônio Barros <antoniohlb@hotmail.com>
2024-04-19 11:07:10 -0300

    Proper first commit

---
+++ Program.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+
+        Position pos = new Position(3, 5);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println(pos);
+    }
+}
